8b659af3f53808e237557070e7bccc371db2ac3a
Daniil <zelenko.danil@gmail.com>
2020-08-02 10:17:04 +0300

    write test to isMiddleware func

---
+++ test/isMiddleware_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	. "../gost"
+	"testing"
+)
+
+func testMiddleware(testModule *testing.T, uri []string, pattern []string) func(toBe bool) {
+	testModule.Log("test uri =", uri, " pattern =", pattern)
+	result := IsMiddleware(uri, pattern)
+	return func(toBe bool) {
+		if result != toBe {
+			testModule.Error("must be", toBe, "then got", result)
+		}
+	}
+}
+
+func TestSimpMiddlewares(t *testing.T) {
+	uri := []string{"article", "1234"}
+	pattern := []string{"article"}
+	testMiddleware(t, uri, pattern)(true)
+
+	uri = []string{"article", "admin", "1234"}
+	pattern = []string{"article", "admin"}
+	testMiddleware(t, uri, pattern)(true)
+
+	uri = []string{"article", "admin", "1234"}
+	pattern = []string{"admin"}
+	testMiddleware(t, uri, pattern)(false)
+}
