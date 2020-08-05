f8b4f7feb5e6c9b7f94cbb0545adb853e52e06b9
Daniil <zelenko.danil@gmail.com>
2020-08-05 11:11:30 +0300

    Remake GetFile. Now it accepts only one arg. Now files are getting by absolute path

--- gost/GetFile.go
+++ gost/GetFile.go
@@ -3,10 +3,16 @@ package lib
 import (
 	"io/ioutil"
 	"log"
+	"os"
 )
 
-func GetFile(dirName, fileName string) string {
-	file, err := ioutil.ReadFile(dirName + "/" + fileName)
+func GetFile(fileName string) string {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	file, err := ioutil.ReadFile(path + fileName)
+
 	if err != nil {
 		log.Fatal(err)
 		return ""
