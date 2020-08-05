595890d66cba336f934fdb6d05b57ec88b5e0301
Daniil <zelenko.danil@gmail.com>
2020-08-05 08:46:13 +0300

    Update func interface.

--- gost/getHandler.go
+++ gost/getHandler.go
@@ -1,17 +1,18 @@
 package lib
 
 import (
+	"errors"
 	"strings"
 )
 
-func GetHandler(handlers handlersMap, uri []string) (handlerCallback, []string) {
-	for handlerPattern, handlerFunc := range handlers {
-		splitedPattern := strings.Split(handlerPattern, "/")[1:]
+func GetHandler(handlers []Handler, uri []string) (Handler, error) {
+	for _, handlerObj := range handlers {
+		splitedPattern := strings.Split(handlerObj.path, "/")[1:]
 		//check path is matching pattern
 		isSuitable := CheckPath(uri, splitedPattern)
 		if isSuitable {
-			return handlerFunc, splitedPattern
+			return handlerObj, nil
 		}
 	}
-	return nil, nil
+	return Handler{}, errors.New("No handler")
 }
