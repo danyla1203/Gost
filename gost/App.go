b47b76b82b0f6ee2522f9ef87e16bb5be63d84b8
Daniil <zelenko.danil@gmail.com>
2020-08-06 11:15:48 +0300

    set params. It gets body from request and modify req object

--- gost/App.go
+++ gost/App.go
@@ -12,13 +12,18 @@ import (
 type Request struct {
 	*http.Request
 	UrlParts map[string]int
+	Body     map[string]string
 }
 type Response struct {
 	http.ResponseWriter
 }
 
 func (res Response) SendFile(path string) {
-	fmt.Fprint(res, GetFile("/assets/"+path))
+	//by default, files to send must be placed in assets dir
+	//TODO: Get correct mime type
+	file, mimeType := GetFile("/assets/" + path)
+	res.Header().Set("Content-Type", mimeType)
+	fmt.Fprint(res, file)
 }
 
 type handlerCallback func(r *Request, res *Response)
@@ -82,12 +87,27 @@ func MakeApp() App {
 	return app
 }
 
+func setParams(req *Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal("something wrong with read data from body")
+	}
+	reducedMap := map[string]string{}
+	for key, value := range req.PostForm {
+		reducedMap[key] = value[0]
+	}
+	req.Body = reducedMap
+}
+
 func (app App) ServeHTTP(socket http.ResponseWriter, request *http.Request) {
 	splitedURI := strings.Split(request.RequestURI, "/")[1:]
 	//handle static, if first part of request uri match static dir name
 	//-> try to get file by second part of request uri
 	if splitedURI[0] == app.staticDirName && len(splitedURI) > 1 {
-		fmt.Fprint(socket, GetFile("/"+app.staticDirName+"/"+splitedURI[1]))
+		//TODO: Get correct mime type
+		file, mimeType := GetFile("/" + app.staticDirName + "/" + splitedURI[1])
+		socket.Header().Set("Content-Type", mimeType)
+		fmt.Fprint(socket, file)
 		return
 	}
 	//get handler and matched pattern
@@ -106,6 +126,7 @@ func (app App) ServeHTTP(socket http.ResponseWriter, request *http.Request) {
 		UrlParts: valuesFromUri,
 	}
 	userResponse := &Response{socket}
+	setParams(userRequest)
 	//execute middlewares and handler
 	for _, callback := range middlewares {
 		callback(userRequest, userResponse)
