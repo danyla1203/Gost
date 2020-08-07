e6860bfa12069bb696aef21efadae3f225394d93
Daniil <zelenko.danil@gmail.com>
2020-08-07 09:52:36 +0300

    If pattern == "/" -> return true (middleware with same pattern must be called before any uri handler)

--- gost/isMiddleware.go
+++ gost/isMiddleware.go
@@ -4,7 +4,9 @@ func IsMiddleware(uri []string, pattern []string) bool {
 	if len(uri) < len(pattern) {
 		return false
 	}
-
+	if len(pattern) == 1 && len(pattern[0]) == 0 {
+		return true
+	}
 	for index, _ := range pattern {
 		if pattern[index] != uri[index] {
 			return false
