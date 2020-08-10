0936ed990badd424a2614e651db80d56f4ca890b
Daniil <zelenko.danil@gmail.com>
2020-08-10 16:46:43 +0300

    Write own redirect

--- gost/Response.go
+++ gost/Response.go
@@ -2,7 +2,6 @@ package lib
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -15,7 +14,11 @@ func (res Response) SendFile(path string) {
 	//TODO: Get correct mime type
 	file, mimeType := GetFile("/assets/" + path)
 	res.Header().Set("Content-Type", mimeType)
-	log.Fatal(fmt.Fprint(res, file))
+	fmt.Fprint(res, file)
+}
+func (res Response) Redirect(location string) {
+	res.Header().Set("Location", location)
+	res.WriteHeader(http.StatusMovedPermanently)
 }
 
 func (res *Response) SetCookie(name, value string) {
